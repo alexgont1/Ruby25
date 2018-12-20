17b037e4280cf9201b9e6dc0a4b866db0614d25b
alexgont1 <alexgont1@gmail.com>
2018-12-19 21:21:16 -0600

    added dataBase for BarberShop

--- app.rb
+++ app.rb
@@ -1,7 +1,15 @@
 require 'sqlite3'
 
+#conect to dataBase
 db = SQLite3::Database.new 'test.sqlite'
 
-puts db.execute "select * from Cars"
+# insert new row
+#db.execute "INSERT INTO Cars (Name, Price) VALUES ('Jaguar', 7000000)"
+
+# show info from dataBase
+db.execute "SELECT * FROM Cars" do |car|
+	puts car
+	puts "======"
+end
 
 db.close
