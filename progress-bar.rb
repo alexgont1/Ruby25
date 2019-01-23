02f6bd96243d95c11b3d53c547ef30f287556b43
alexgont1 <alexgont1@gmail.com>
2019-01-22 18:55:23 -0600

    simple progress bar

---
+++ progress-bar.rb
@@ -0,0 +1,13 @@
+i = 0
+
+while i < 1000  do
+   print("\\"); sleep 0.2; print("\r" )
+
+   print("|" ); sleep 0.2; print("\r" )   
+
+   print("/" ); sleep 0.2; print("\r" )
+
+   print("-" ); sleep 0.2; print("\r" )
+
+   i +=1
+end
