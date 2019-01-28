bc0a3476f8db0db717ce7254c4f7e8f1a447df35
alexgont1@gmail.com <alexgont1@gmail.com>
2019-01-27 18:49:11 -0600

    hash demo 3

---
+++ hash3.rb
@@ -0,0 +1,29 @@
+hh1 = {10 => 1, 15 => 2, 20 => 3}
+
+hh2 = {'cat' => ['better than dog', 'says meo', 'purrs'], 
+	'dog' => ['woof!', 'angry', 'not cat', 'big animal'], 
+	'book' => ['many pages', 'to read']}
+
+
+puts 'hh.keys'
+puts hh1.keys
+puts
+puts hh2.keys
+
+puts
+puts 'hh.keys.inspect'
+puts hh1.keys.inspect
+puts
+puts hh2.keys.inspect
+
+puts
+puts 'hh.values'
+puts hh1.values
+puts
+puts hh2.values
+
+puts
+puts 'hh.values.inspect'
+puts hh1.values.inspect
+puts
+puts hh2.values.inspect
