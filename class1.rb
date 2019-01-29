e54c295d1e8f3f1fcb5a5cd623563fad1d6a5b86
alexgont1@gmail.com <alexgont1@gmail.com>
2019-01-28 20:04:30 -0600

    class demo1

---
+++ class1.rb
@@ -0,0 +1,39 @@
+class Book
+
+	def initialize #work only 1 time when we create new instance
+		@hh = {} #accesible only in this class
+	end
+
+	#to get access to @hh:
+	def aaa
+		@hh
+	end
+
+	def add_person options #options must be in the end of string!!!
+		(puts 'Already exists! Won\'t be added.'; return) if @hh[options[:name]]
+		@hh[options[:name]] = options[:age]
+	end
+		
+	def show_all
+		@hh.each_key do |key|
+			puts "Name is #{key} and age is #{@hh[key]}"
+		end
+	end
+
+end
+
+b = Book.new
+
+loop do
+	print 'Enter name or press [Enter] to exit: '
+	name = gets.strip.capitalize
+
+	(b.show_all; break) if name == ''
+		
+	print 'Enter age: '
+	age = gets.to_i
+
+	b.add_person :name => name, :age => age	
+end
+
+puts b.aaa
