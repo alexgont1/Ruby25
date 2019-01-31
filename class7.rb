4d0955e7785a40608b2e4fce60f40e9561e98294
alexgont1@gmail.com <alexgont1@gmail.com>
2019-01-30 20:28:37 -0600

    class demo7

---
+++ class7.rb
@@ -0,0 +1,12 @@
+class Song
+	attr_reader :name, :artist, :duration     
+	def initialize (name, artist, duration)
+    	@name     = name
+		@artist   = artist
+		@duration = duration
+  	end
+end
+aSong = Song.new("Bicylops", "Fleck", 260)
+puts aSong.name
+puts aSong.artist
+puts aSong.duration
